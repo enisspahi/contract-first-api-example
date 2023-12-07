cbe49b229f7a04224252dd9eb25eb0ad8704ada4
enisspahi <enis.spahi@openvalue.de>
2023-12-07 15:58:33 +0100

    cors configuration

--- service/src/main/java/com/enisspahi/example/CorsConfigurer.java
+++ service/src/main/java/com/enisspahi/example/CorsConfigurer.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://enisspahi.github.io");
+        registry.addMapping("/**").allowedOrigins("https://enisspahi.github.io", "https://editor.swagger.io/");
     }
 }
 
